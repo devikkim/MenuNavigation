5a526ae2c9808c338ee6fb0a9a3899cd0fc138d8
James <devikkim@gmail.com>
2019-06-15 15:51:05 +0900

    Initial Commit

---
+++ AddMenu/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AddMenu
+//
+//  Created by InKwon Devik Kim on 15/06/2019.
+//  Copyright © 2019 InKwon Devik Kim. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
